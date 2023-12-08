80a26896bd4ad0a373a26b9bdd8df40c344419a3
Michelle <thainara.lopesbrito@yahoo.com>
2023-12-08 09:29:40 -0300

    StylesLopes

---
+++ js/carrinho.js
@@ -0,0 +1,81 @@
+// // Função para exibir produtos no carrinho
+// function exibirCarrinho() {
+//     // Obtém o carrinho da sessão
+//     let carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
+//     let listaCarrinho = document.getElementById('lista-carrinho');
+//     let totalCarrinho = document.getElementById('total-carrinho');
+  
+//     // Limpa a lista antes de adicionar os produtos
+//     listaCarrinho.innerHTML = '';
+  
+//     // Adiciona os produtos ao carrinho
+//     carrinho.forEach(produto => {
+//       let li = document.createElement('li');
+//       li.textContent = `${produto.nome} - R$ ${produto.preco.toFixed(2)}`;
+//       listaCarrinho.appendChild(li);
+//     });
+  
+//     // Calcula o total do carrinho
+//     let total = carrinho.reduce((acumulador, produto) => acumulador + produto.preco, 0);
+  
+//     // Exibe o total na página
+//     totalCarrinho.textContent = `Total: R$ ${total.toFixed(2)}`;
+//   }
+  
+//   // Função para limpar o carrinho
+//   function limparCarrinho() {
+//     // Limpa o carrinho na sessão
+//     sessionStorage.removeItem('carrinho');
+//     // Atualiza a exibição do carrinho
+//     exibirCarrinho();
+//   }
+  
+//   // Exibe o carrinho assim que a página é carregada
+//   exibirCarrinho();
+  
+// Função para exibir produtos no carrinho
+function exibirCarrinho() {
+    // Obtém o carrinho da sessão
+    let carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
+    let listaCarrinho = document.getElementById('lista-carrinho');
+    let totalCarrinho = document.getElementById('total-carrinho');
+  
+    // Limpa a lista antes de adicionar os produtos
+    listaCarrinho.innerHTML = '';
+  
+    // Adiciona os produtos ao carrinho
+    carrinho.forEach(produto => {
+      let li = document.createElement('li');
+  
+      // Adiciona a imagem do produto
+      let img = document.createElement('img');
+      img.src = produto.imagem;
+      img.alt = produto.nome;
+      img.style.width = '50px'; // Defina a largura desejada para a imagem
+      li.appendChild(img);
+  
+      // Adiciona o nome e preço do produto
+      li.innerHTML += ` ${produto.nome} - R$ ${produto.preco.toFixed(2)}`;
+      
+      listaCarrinho.appendChild(li);
+    });
+  
+    // Calcula o total do carrinho
+    let total = carrinho.reduce((acumulador, produto) => acumulador + produto.preco, 0);
+  
+    // Exibe o total na página
+    totalCarrinho.textContent = `Total: R$ ${total.toFixed(2)}`;
+  }
+  
+  // Função para limpar o carrinho
+  function limparCarrinho() {
+    // Limpa o carrinho na sessão
+    sessionStorage.removeItem('carrinho');
+    // Atualiza a exibição do carrinho
+    exibirCarrinho();
+  }
+  
+  // Exibe o carrinho assim que a página é carregada
+  exibirCarrinho();
+  
+
---
+++ js/catalogo.js
@@ -0,0 +1,15 @@
+// ... (código existente) ...
+
+// Função para adicionar produtos ao carrinho
+function adicionarAoCarrinho(nomeProduto, precoProduto, imagemProduto) {
+  // Verifica se existe um carrinho na sessão
+  let carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
+
+  // Adiciona o produto ao carrinho
+  carrinho.push({ nome: nomeProduto, preco: precoProduto, imagem: imagemProduto });
+
+  // Salva o carrinho na sessão
+  sessionStorage.setItem('carrinho', JSON.stringify(carrinho));
+}
+
+// ... (código existente) ...
