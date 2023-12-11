e10f93868663b2495ffa0a59deea9c3e4b27217b
Michelle <thainara.lopesbrito@yahoo.com>
2023-12-11 11:49:14 -0300

    StylesLopes

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,47 +1,105 @@
-
-// Função para exibir produtos no carrinho
+  // Função para exibir produtos no carrinho
 function exibirCarrinho() {
-    // Obtém o carrinho da sessão
-    let carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
-    let listaCarrinho = document.getElementById('lista-carrinho');
-    let totalCarrinho = document.getElementById('total-carrinho');
-  
-    // Limpa a lista antes de adicionar os produtos
-    listaCarrinho.innerHTML = '';
-  
-    // Adiciona os produtos ao carrinho
-    carrinho.forEach(produto => {
-      let li = document.createElement('li');
-  
-      // Adiciona a imagem do produto
-      let img = document.createElement('img');
-      img.src = produto.imagem;
-      img.alt = produto.nome;
-      img.style.width = '50px'; // Defina a largura desejada para a imagem
-      li.appendChild(img);
-  
-      // Adiciona o nome e preço do produto
-      li.innerHTML += ` ${produto.nome} - R$ ${produto.preco.toFixed(2)}`;
-      
-      listaCarrinho.appendChild(li);
-    });
-  
-    // Calcula o total do carrinho
-    let total = carrinho.reduce((acumulador, produto) => acumulador + produto.preco, 0);
-  
-    // Exibe o total na página
-    totalCarrinho.textContent = `Total: R$ ${total.toFixed(2)}`;
-  }
-  
-  // Função para limpar o carrinho
-  function limparCarrinho() {
-    // Limpa o carrinho na sessão
-    sessionStorage.removeItem('carrinho');
-    // Atualiza a exibição do carrinho
-    exibirCarrinho();
-  }
-  
-  // Exibe o carrinho assim que a página é carregada
-  exibirCarrinho();
-  
+  // Obtém o carrinho da sessão
+  let carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
+  let listaCarrinho = document.getElementById('lista-carrinho');
+  let totalCarrinho = document.getElementById('total-carrinho');
 
+  // Limpa a lista antes de adicionar os produtos
+  listaCarrinho.innerHTML = '';
+
+  // Adiciona os produtos ao carrinho
+  carrinho.forEach((produto, index) => {
+    let li = document.createElement('li');
+    li.className = 'd-flex justify-content-between align-items-center';
+
+    // Adiciona a imagem do produto
+    let img = document.createElement('img');
+    img.src = produto.imagem;
+    img.alt = produto.nome;
+    img.style.width = '50px'; // Defina a largura desejada para a imagem
+    li.appendChild(img);
+
+    // Adiciona o nome e preço do produto
+    li.innerHTML += ` ${produto.nome} - R$ ${produto.preco.toFixed(2)}`;
+
+    // Adiciona o campo de quantidade
+    let quantidadeInput = document.createElement('input');
+    quantidadeInput.type = 'number';
+    quantidadeInput.min = 1;
+    quantidadeInput.value = produto.quantidade || 1; // Valor padrão 1 se não houver quantidade definida
+    quantidadeInput.addEventListener('change', function () {
+      // Atualiza a quantidade no carrinho quando o valor do input é alterado
+      atualizarQuantidade(index, parseInt(this.value, 10));
+    });
+    li.appendChild(quantidadeInput);
+
+    // Adiciona o botão de remoção
+    let botaoRemover = document.createElement('button');
+    botaoRemover.type = 'button';
+    botaoRemover.className = 'btn btn-danger btn-sm';
+    botaoRemover.innerText = 'Remover';
+    botaoRemover.onclick = function () {
+      // Chame uma função para remover o item do carrinho
+      removerDoCarrinho(index);
+    };
+    li.appendChild(botaoRemover);
+
+    listaCarrinho.appendChild(li);
+  });
+
+  // Calcula o total do carrinho
+  let total = carrinho.reduce((acumulador, produto) => acumulador + produto.preco * (produto.quantidade || 1), 0);
+
+  // Exibe o total na página
+  totalCarrinho.textContent = `Total: R$ ${total.toFixed(2)}`;
+}
+
+// Função para remover um item do carrinho
+function removerDoCarrinho(index) {
+  // Obtém o carrinho da sessão
+  let carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
+
+  // Remove o item pelo índice
+  carrinho.splice(index, 1);
+
+  // Salva o carrinho atualizado na sessão
+  sessionStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+  // Atualiza a exibição do carrinho
+  exibirCarrinho();
+}
+
+// Função para atualizar a quantidade de um item no carrinho
+function atualizarQuantidade(index, novaQuantidade) {
+  // Obtém o carrinho da sessão
+  let carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
+
+  // Atualiza a quantidade do item pelo índice
+  carrinho[index].quantidade = novaQuantidade;
+
+  // Salva o carrinho atualizado na sessão
+  sessionStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+  // Atualiza a exibição do carrinho
+  exibirCarrinho();
+}
+
+// Exibe o carrinho assim que a página é carregada
+exibirCarrinho();
+
+
+// Função para limpar o carrinho
+function limparCarrinho() {
+  // Limpa o carrinho na sessão
+  sessionStorage.removeItem('carrinho');
+
+  // Atualiza a exibição do carrinho
+  exibirCarrinho();
+}
+
+
+function exibirAlertaCompra() {
+  alert('Parabéns! Sua compra foi realizada com sucesso.');
+  // Adicione aqui o código para realizar a compra ou redirecionar o usuário, se necessário.
+}
--- js/catalogo.js
+++ js/catalogo.js
@@ -1,16 +1,76 @@
-// ... (código existente) ...
-
-// Função para adicionar produtos ao carrinho
-function adicionarAoCarrinho(nomeProduto, precoProduto, imagemProduto) {
-  // Verifica se existe um carrinho na sessão
-  let carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
-
-  // Adiciona o produto ao carrinho
-  carrinho.push({ nome: nomeProduto, preco: precoProduto, imagem: imagemProduto });
 
 
-  // Salva o carrinho na sessão
-  sessionStorage.setItem('carrinho', JSON.stringify(carrinho));
-}
+    // Função para adicionar produtos ao carrinho
+    function adicionarAoCarrinho(nomeProduto, precoProduto, imagemProduto) {
+      // Verifica se existe um carrinho na sessão
+      let carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
 
-// ... (código existente) ...
+      // Verifica se o produto já está no carrinho
+      let produtoExistente = carrinho.find(produto => produto.nome === nomeProduto);
+
+      if (produtoExistente) {
+        // Se o produto já existe, aumenta a quantidade
+        produtoExistente.quantidade = (produtoExistente.quantidade || 1) + 1;
+      } else {
+        // Se o produto não existe, adiciona ao carrinho
+        carrinho.push({ nome: nomeProduto, preco: precoProduto, imagem: imagemProduto, quantidade: 1 });
+
+        // Adiciona o item ao carrinho visualmente
+        adicionarItemVisualmente(nomeProduto, precoProduto, imagemProduto);
+      }
+
+      // Salva o carrinho na sessão
+      sessionStorage.setItem('carrinho', JSON.stringify(carrinho));
+    }
+
+    // Função para adicionar visualmente um item ao carrinho
+    function adicionarItemVisualmente(nomeProduto, precoProduto, imagemProduto) {
+      let listaCarrinho = document.getElementById('lista-carrinho');
+
+      // Cria um novo item na lista visual do carrinho
+      let li = document.createElement('li');
+      li.className = 'd-flex justify-content-between align-items-center';
+
+      // Adiciona a imagem do produto
+      let img = document.createElement('img');
+      img.src = imagemProduto;
+      img.alt = nomeProduto;
+      img.style.width = '50px'; // Defina a largura desejada para a imagem
+      li.appendChild(img);
+
+      // Adiciona o nome e preço do produto
+      li.innerHTML += ` ${nomeProduto} - R$ ${precoProduto.toFixed(2)}`;
+
+      // Adiciona o campo de quantidade
+      let quantidadeInput = document.createElement('input');
+      quantidadeInput.type = 'number';
+      quantidadeInput.min = 1;
+      quantidadeInput.value = 1; // Valor padrão 1 para um novo item
+      quantidadeInput.addEventListener('change', function () {
+        // Atualiza a quantidade no carrinho quando o valor do input é alterado
+        // (não é necessário no momento para novos itens, mas pode ser útil no futuro)
+      });
+      li.appendChild(quantidadeInput);
+
+      // Adiciona o botão de remoção
+      let botaoRemover = document.createElement('button');
+      botaoRemover.type = 'button';
+      botaoRemover.className = 'btn btn-danger btn-sm';
+      botaoRemover.innerText = 'Remover';
+      botaoRemover.onclick = function () {
+        // Chame uma função para remover o item do carrinho
+        removerDoCarrinhoVisualmente(li);
+      };
+      li.appendChild(botaoRemover);
+
+      // Adiciona o novo item à lista de carrinho
+      listaCarrinho.appendChild(li);
+    }
+
+    // Função para remover visualmente um item do carrinho
+    function removerDoCarrinhoVisualmente(item) {
+      // Remove o item da lista visual do carrinho
+      document.getElementById('lista-carrinho').removeChild(item);
+    }
+ 
+
